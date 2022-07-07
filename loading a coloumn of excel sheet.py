387d1d30d702348ad7410dead471b1c151ddf872
Sushant Thakur <106450499+sushantst@users.noreply.github.com>
2022-07-07 10:46:27 +0530

    Update loading a coloumn of excel sheet.py

--- loading a coloumn of excel sheet.py
+++ loading a coloumn of excel sheet.py
@@ -1,5 +1,6 @@
 import pandas as pd
 file=pd.read_excel('sampl.xls')
+# Rep is one of the column of the data set in our excel file named sampl.xls and jardine is one of a name in that list till which our for loop will run.
 column_a=file['Rep']
 for um in column_a:
 	if um =='Jardine':
