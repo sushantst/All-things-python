2450294b3ce9cbf7edf4675cf3a4dbc7112cdd94
Sushant Thakur <106450499+sushantst@users.noreply.github.com>
2022-08-04 23:00:45 +0530

    Update email.py

--- email.py
+++ email.py
@@ -2,6 +2,6 @@ import smtplib
 smtobj=smtplib.SMTP("smtp.gmail.com", 587)
 smtobj.ehlo()
 smtobj.starttls()
-smtobj.login("Rmessona@gmail.com","ME5048@1st")
+smtobj.login("Rmessona@gmail.com","wwe")
 smtobj.sendmail("Rmessona@gmail.com","sushantst15@gmail.com","Subject: SMPT check.\n just checking to see if my mail is being send via python smtp protocol ")
 smtobj.quit()
